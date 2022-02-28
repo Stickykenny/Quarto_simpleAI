ea34742a987d36f3a89c74fa216d6c88df661572
Jeremy <kennykill@outlook.fr>
2022-02-28 18:10:25 +0100

    Création des pièces et début du plateau

--- game/piece.py
+++ game/piece.py
@@ -2,3 +2,48 @@
 Classe qui définit et instancie les pièces du jeux de Quarto
 """
 
+class Piece :
+
+    def __init__(self, id, color, size, form, top) :
+        """
+        color black = True
+        size tall = True
+        form square = True
+        top hollow = True
+        """
+        self.id = id
+        self.black = color
+        self.tall = size
+        self.square = form
+        self.hollow = top
+
+    def affichePiece(self) :
+        print(self.id, self.black, self.tall, self.square, self.hollow)
+
+
+
+#Instancie les pièces
+pieces = [
+Piece("0",  False ,False, False, False),
+Piece("1",  False ,False, False, True ),
+Piece("2",  False ,False, True , False),
+Piece("3",  False ,False, True , True ),
+Piece("4",  False ,True , False, False),
+Piece("5",  False ,True , False, True ),
+Piece("6",  False ,True , True , False),
+Piece("7",  False ,True , True , True ),
+    
+Piece("8" , True  ,False, False, False),
+Piece("9" , True  ,False, False, True ),
+Piece("10", True  ,False, True , False),
+Piece("11", True  ,False, True , True ),
+Piece("12", True  ,True , False, False),
+Piece("13", True  ,True , False, True ),
+Piece("14", True  ,True , True , False),
+Piece("15", True  ,True , True , True )
+]
+
+"""
+for j in pieces :
+    j.affichePiece()
+"""
--- game/plateau.py
+++ game/plateau.py
@@ -1,10 +1,26 @@
+from piece import pieces
 class Board :
     def __init__(self) :
         self.board = [[None for i in range(4)] for j in range(4)]
 
-    def placerPiece(self, piece) :
+    def placerPiece(self, piece, x,y) :
+        self.board[x][y] = piece.id
         return 0
 
     def checkState(self, piece, x, y) :
         """Vérifie si placer une pièce en x y fait terminer la game"""
         return False
+
+    def showGrid(self) :
+        """Affiche et renvoi la grille"""
+        for i in range(len(self.board)) :
+            print(self.board[i])
+        return self.board
+
+b= Board()
+b.showGrid()
+print("\n \nafter putting some pieces")
+b.placerPiece(pieces[1], 0,2)
+
+b.placerPiece(pieces[8], 3,3)
+b.showGrid()
