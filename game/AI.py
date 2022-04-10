52afe6a394debd7c85dd47a298c09e5f589dc4e0
Jeremy <kennykill@outlook.fr>
2022-04-10 14:24:04 +0200

    Advancing AI,\n TODO : create a proper eval() function and complete the Minimax

--- game/AI.py
+++ game/AI.py
@@ -1,4 +1,5 @@
 import random
+import plateau
 class AI :
     def __init__(self, difficulty) :
         self.difficulty = difficulty
@@ -26,3 +27,41 @@ class AI :
             #TODO
             return random.choice(board.getAvailable)
 
+
+    def miniMax(self, board, depth) :
+        #return the a in Action(state) maximaxing Min_value(result(a,state))
+        pass
+
+    def maxValue(self, board,depth) :
+        if depth <= 0 :
+            return self.evalValue(board)
+        v = float("-inf")
+        for a,s in self.sucessors() :
+            v = max(v, self.minValue(s))
+        return v
+        pass
+
+    def minValue(self, board,depth) :
+        if depth <= 0 :
+            return -self.evalValue(board)
+        v = float("inf")
+        for a,s in self.sucessors() :
+            v = max(v, self.maxValue(s))
+        return v
+        pass
+
+    def evalValue(self, board, line, column):
+
+        #Case : Victory on this board
+        if board.checkState(board, line, column) :
+            return float("inf")
+
+
+        #here calculate eval 
+        g = board.getGrid
+        for i in range(4):
+            pass
+        pass
+
+    def sucessors(self , board):
+        pass
